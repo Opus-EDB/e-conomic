01cf95a506f5fc507e9ffb8f9877429015638ad1
Martin Dobbelaar <mado@opusedb.dk>
2025-08-04 15:08:58 +0200

    Create or update dimensions

--- dimensions.go
+++ dimensions.go
@@ -8,12 +8,20 @@ import (
 
 const DIMENSIONAPI_BASE = "/dimensionsapi/v5.0.0"
 
+type dimension struct {
+	Active          bool   `json:"active"`
+	DimensionNumber int    `json:"dimensionNumber"`
+	Key             int    `json:"key"`
+	Name            string `json:"name"`
+	ObjectVersion   string `json:"objectVersion,omitempty"`
+}
+
 func (client *Client) CreateDimensionValue(number, key int, name string) error {
-	body := map[string]any{
-		"active":          true,
-		"dimensionNumber": number,
-		"key":             key,
-		"name":            name,
+	body := dimension{
+		Active:          true,
+		DimensionNumber: number,
+		Key:             key,
+		Name:            name,
 	}
 	return client.callAPI(DIMENSIONAPI_BASE+"/values", http.MethodPost, nil, body, nil)
 }
@@ -33,6 +41,31 @@ func (client *Client) CreateDimensionValueIfItDoesNotExist(number, key int, name
 	return true, client.CreateDimensionValue(number, key, name)
 }
 
+// Updates or creates a dimension value.
+// Returns true if the value was created, or false if it already exists.
+func (client *Client) CreateOrUpdateDimensionValue(number, key int, name string) (bool, error) {
+	var existingDimension dimension
+	err := client.callAPI(fmt.Sprintf(DIMENSIONAPI_BASE+"/values/%d/%d", number, key), http.MethodGet, nil, nil, &existingDimension)
+	if err == nil {
+		return client.UpdateDimensionValue(number, key, name, existingDimension.ObjectVersion)
+	}
+	// XXX TODO nicer 404 handling
+	if !strings.Contains(err.Error(), "not found") {
+		return false, err
+	}
+	return true, client.CreateDimensionValue(number, key, name)
+}
+
+func (client *Client) UpdateDimensionValue(number, key int, name, objectVersion string) (bool, error) {
+	body := dimension{
+		Active:          true,
+		DimensionNumber: number,
+		Key:             key,
+		Name:            name,
+		ObjectVersion:   objectVersion,
+	}
+	return false, client.callAPI(DIMENSIONAPI_BASE+"/values", http.MethodPut, nil, body, nil)
+}
 func (client *Client) AddDimensionValueToDraftEntry(dimensionNumber, dimensionKey, journalNumber, entryNumber int) error {
 	body := map[string]any{
 		"dimensionNumber": dimensionNumber,
